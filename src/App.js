a8d697b5762e0775b8f57f3773487ad871c2f63e
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-07 14:39:22 +0200

    route components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Navbar";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
 function App() {
   return (
-    <div>
-      <h1>Youtube Clone</h1>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />}></Route>
+          <Route path="/video/:id/" element={<VideoDetail />}></Route>
+          <Route path="/channel/:id/" element={<ChannelDetail />}></Route>
+          <Route path="/search/:searchTerm/" element={<SearchFeed />}></Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
